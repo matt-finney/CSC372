c230a3bb65afe7160be09b9be0781c37cc585020
Matt <matthew.finney@hotmail.com>
2025-10-10 12:26:57 -0400

    Update test cases

--- src/CSC372Mod1/Module1.java
+++ src/CSC372Mod1/Module1.java
@@ -3,23 +3,23 @@ package CSC372Mod1;
 public class Module1 {
     public static void main(String[] args) {
         // create a BankAccount
-        BankAccount bankAccount = new BankAccount("John", "Doe", 12345);
-        bankAccount.deposit(500);
+        BankAccount bankAccount = new BankAccount("Mike", "Brownley", 55789);
+        bankAccount.deposit(850);
         System.out.println("Bank Account Summary:");
         bankAccount.accountSummary();
-        bankAccount.withdrawal(100);
-        System.out.println("\nAfter withdrawal of $100:");
+        bankAccount.withdrawal(275);
+        System.out.println("\nAfter withdrawal of $275:");
         bankAccount.accountSummary();
         
         System.out.println("\n-----------------------");
         
         // create a CheckingAccount
-        CheckingAccount checkingAccount = new CheckingAccount("Jane", "Smith", 67890, 1.5);
-        checkingAccount.deposit(300);
+        CheckingAccount checkingAccount = new CheckingAccount("Maria", "Garcia", 11223, 3.75);
+        checkingAccount.deposit(200);
         System.out.println("\nChecking Account Summary:");
         checkingAccount.displayAccount();
-        checkingAccount.processWithdrawal(350); // will trigger overdraft
-        System.out.println("\nAfter withdrawal of $350:");
+        checkingAccount.processWithdrawal(300); // will trigger overdraft
+        System.out.println("\nAfter withdrawal of $300:");
         checkingAccount.displayAccount();
     }
 }
