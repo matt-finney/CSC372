8df49083df2889c240ad9c631b1a4ab8050431f7
Matt <matthew.finney@hotmail.com>
2025-10-10 12:15:18 -0400

    Showcases interactions with BankAccount and CheckingAccount

--- src/CSC372Mod1/Module1.java
+++ src/CSC372Mod1/Module1.java
@@ -1,5 +1,25 @@
 package CSC372Mod1;
 
 public class Module1 {
-
+    public static void main(String[] args) {
+        // create a BankAccount
+        BankAccount bankAccount = new BankAccount("John", "Doe", 12345);
+        bankAccount.deposit(500);
+        System.out.println("Bank Account Summary:");
+        bankAccount.accountSummary();
+        bankAccount.withdrawal(100);
+        System.out.println("\nAfter withdrawal of $100:");
+        bankAccount.accountSummary();
+        
+        System.out.println("\n-----------------------");
+        
+        // create a CheckingAccount
+        CheckingAccount checkingAccount = new CheckingAccount("Jane", "Smith", 67890, 1.5);
+        checkingAccount.deposit(300);
+        System.out.println("\nChecking Account Summary:");
+        checkingAccount.displayAccount();
+        checkingAccount.processWithdrawal(350); // will trigger overdraft
+        System.out.println("\nAfter withdrawal of $350:");
+        checkingAccount.displayAccount();
+    }
 }
