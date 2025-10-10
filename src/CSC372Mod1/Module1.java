c4afbeb6f85712a0fe82825b7bf0199a770c1ebb
Matt <matthew.finney@hotmail.com>
2025-10-10 12:32:55 -0400

    Change spacing and new test use cases

--- src/CSC372Mod1/Module1.java
+++ src/CSC372Mod1/Module1.java
@@ -1,25 +1,27 @@
 package CSC372Mod1;
 
-public class Module1 {
-    public static void main(String[] args) {
+public class Module1 
+{
+    public static void main(String[] args) 
+    {
         // create a BankAccount
-        BankAccount bankAccount = new BankAccount("Mike", "Brownley", 55789);
-        bankAccount.deposit(850);
+        BankAccount bankAccount = new BankAccount("Ava", "Thompson", 90123);
+        bankAccount.deposit(920);
         System.out.println("Bank Account Summary:");
         bankAccount.accountSummary();
-        bankAccount.withdrawal(275);
-        System.out.println("\nAfter withdrawal of $275:");
+        bankAccount.withdrawal(345);
+        System.out.println("\nAfter withdrawal of $345:");
         bankAccount.accountSummary();
         
         System.out.println("\n-----------------------");
         
         // create a CheckingAccount
-        CheckingAccount checkingAccount = new CheckingAccount("Maria", "Garcia", 11223, 3.75);
-        checkingAccount.deposit(200);
+        CheckingAccount checkingAccount = new CheckingAccount("Liam", "Neeson", 33445, 2.10);
+        checkingAccount.deposit(180);
         System.out.println("\nChecking Account Summary:");
         checkingAccount.displayAccount();
-        checkingAccount.processWithdrawal(300); // will trigger overdraft
-        System.out.println("\nAfter withdrawal of $300:");
+        checkingAccount.processWithdrawal(340); // will trigger overdraft
+        System.out.println("\nAfter withdrawal of $340:");
         checkingAccount.displayAccount();
     }
 }
