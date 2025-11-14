d68f0f8b787de3b1a7c7b8f1d67765a2962bfa3c
Matt <matthew.finney@hotmail.com>
2025-11-14 10:23:18 -0500

    add NameComparator to compare Student objects by name

---
+++ src/CSC372Mod6/NameComparator.java
@@ -0,0 +1,13 @@
+package CSC372Mod6;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Student> 
+{
+    @Override
+    public int compare(Student s1, Student s2) 
+    {
+        return s1.getName().compareToIgnoreCase(s2.getName());
+    }
+}
+
